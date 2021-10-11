7db6124d2ffacc10aaac97c2c843802095e311c7
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-10-11 11:29:33 +0530

    Before adding lombok

---
+++ src/main/java/com/atb/springboot/tutorial/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.atb.springboot.tutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    /*@RequestMapping(value="/",method = RequestMethod.GET)
+    public String helloWorld(){
+        return "Welcome to ATB Tutorials";
+    }*/
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome to ATB Tutorials !***!";
+    }
+
+
+
+}
